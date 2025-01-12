4aa0fae08bcb64a970817b70b30c101a29092bb0
Mauricio Medina <mau172001mece@gmail.com>
2025-01-12 11:30:25 -0600

    nueva clase que almacena las rutas principales de la API

---
+++ lib/config/endpoints/enpoints.dart
@@ -0,0 +1,7 @@
+class Enpoints {
+  static const String baseApiURL = 'http://10.0.2.2:8081';
+  static const login = "$baseApiURL/login/";
+  static const logout = "$baseApiURL/logout/";
+  static const careersAPI = "$baseApiURL/users/careers/";
+  static const groupsAPI = "$baseApiURL/courses/groups/";
+}
