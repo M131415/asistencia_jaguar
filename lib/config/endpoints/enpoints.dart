d9f789d38fae169699a48baef28703dec33c53ff
Mauricio Medina <mau172001mece@gmail.com>
2025-01-23 16:54:35 -0600

    se agregaron mas rutas de la API

--- lib/config/endpoints/enpoints.dart
+++ lib/config/endpoints/enpoints.dart
@@ -1,9 +1,17 @@
 class Enpoints {
-  static const String baseApiURL = 'http://10.0.2.2:8081';
-  static const refreshToken = "$baseApiURL/api/token/refresh/";
-  static const login = "$baseApiURL/login/";
-  static const logout = "$baseApiURL/logout/";
-  static const careersAPI = "$baseApiURL/users/careers/";
-  static const schoolRoomsAPI = "$baseApiURL/courses/school_rooms/";
-  static const groupsAPI = "$baseApiURL/courses/groups/";
+  static const String baseApiURL     = 'http://10.0.2.2:8081';
+  static const String refreshToken   = "$baseApiURL/api/token/refresh/";
+  static const String login          = "$baseApiURL/login/";
+  static const String logout         = "$baseApiURL/logout/";
+  static const String careersAPI     = "$baseApiURL/users/careers/";
+  static const String usersAPI       = "$baseApiURL/users/users/";
+  static const String schoolRoomsAPI = "$baseApiURL/courses/school_rooms/";
+  static const String subjectsAPI    = "$baseApiURL/courses/subjects/";
+  static const String departments    = "$baseApiURL/courses/departments/";
+  static const String periodsAPI     = "$baseApiURL/courses/periods/";
+  static const String schedulesAPI   = "$baseApiURL/courses/schedules/";
+  static const String groupsAPI      = "$baseApiURL/courses/groups/";
+  static const String coursesAPI     = "$baseApiURL/courses/courses/";
+  static const String attendanceAPI  = "$baseApiURL/attendance/attendances/";
+  static const String enrollmentsAPI = "$baseApiURL/attendance/enrollments/";
 }
