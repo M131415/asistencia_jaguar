ae99fc9cee4dc25e92c1777aeefd4cadb9c12b76
Mauricio Medina <mau172001mece@gmail.com>
2025-01-23 17:08:16 -0600

    se agrego el metodo toSpanish para el UserRol enum

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -9,17 +9,32 @@ enum UserRol {
   /// Método estático para convertir una cadena a un valor del enum
   static UserRol fromString(String value) {
     switch (value.toUpperCase()) {
-      case 'ADMIN':
+      case 'ADMIN' || 'ADMINISTRADOR':
         return UserRol.admin;
-      case 'TEACHER':
+      case 'TEACHER' || 'DOCENTE':
         return UserRol.teacher;
-      case 'STUDENT':
+      case 'STUDENT' || 'ESTUDIANTE':
         return UserRol.student;
       default:
         return UserRol.none;
     }
   }
+  // Método para convertir un valor del enum a una cadena
   String toJson() => name.toUpperCase();
+
+  // Método para obtener el nombre del rol en español
+  String toSpanish() {
+    switch (this) {
+      case UserRol.admin:
+        return 'Administrador';
+      case UserRol.teacher:
+        return 'Docente';
+      case UserRol.student:
+        return 'Estudiante';
+      default:
+        return 'Sin rol';
+    }
+  }
 }
 
 class User extends Equatable {
